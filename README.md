1677ffbfb643186afe844eb884d6807900e6a129
nokaa <nokaa@cock.li>
2016-04-01 23:38:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# git-your-life-back
+
+A userscript to remove the GitHub contributions graph.
+Some people find that the contributions graph creates stress because they
+feel the need to keep up their streak.
---
+++ git-your-life-back.js
@@ -0,0 +1,18 @@
+// Copyright (C)  2016 nokaa <nokaa@cock.li>
+// This software is licensed under the terms of the
+// GNU General Public License version 3. You should have
+// received a copy of this license with this software.
+// The license may also be found at https://gnu.org/licenses/gpl.txt
+
+// ==UserScript==
+// @name        git-your-life-back
+// @version     1.0.0
+// @namespace   git-your-life-back
+// @description Removes the Github contribution graph
+// @license     GPLv3, https://gnu.org/licenses/gpl.txt
+// @include     https://github.com/*
+// @include     http://github.com/*
+// @grant       none
+// ==/UserScript==
+
+document.getElementById("contributions-calendar").parentElement.remove();
